63b2b9ec531684095be579a6221371d4f5c24166
Sumit Dalakiya <sumit.magespider@gmail.com>
2019-07-30 18:55:43 +0530

    Verification Otp design done.

--- app/src/main/java/com/mage/ziplrdelivery/common/Screen.java
+++ app/src/main/java/com/mage/ziplrdelivery/common/Screen.java
@@ -2,12 +2,16 @@ package com.mage.ziplrdelivery.common;
 
 import com.mage.ziplrdelivery.ui.LoginMainActivity;
 import com.mage.ziplrdelivery.ui.MobileNoActivity;
+import com.mage.ziplrdelivery.ui.PasswordActivity;
 import com.mage.ziplrdelivery.ui.RegistrationActivity;
 import com.mage.ziplrdelivery.ui.SplashActivity;
+import com.mage.ziplrdelivery.ui.VerificationActivity;
 
 public class Screen {
     public static final String SPLASH_ACTIVITY = SplashActivity.class.getSimpleName();
     public static final String LOGIN_MAIN_ACTIVITY = LoginMainActivity.class.getSimpleName();
     public static final String REGISTRATION_ACTIVITY = RegistrationActivity.class.getSimpleName();
     public static final String MOBILE_NO_ACTIVITY = MobileNoActivity.class.getSimpleName();
+    public static final String PASSWORD_ACTIVITY = PasswordActivity.class.getSimpleName();
+    public static final String VERIFICATION_ACTIVITY = VerificationActivity.class.getSimpleName();
 }
--- app/src/main/java/com/mage/ziplrdelivery/ui/VerificationActivity.java
+++ app/src/main/java/com/mage/ziplrdelivery/ui/VerificationActivity.java
@@ -1,43 +1,43 @@
 package com.mage.ziplrdelivery.ui;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Paint;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.databinding.DataBindingUtil;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.mage.ziplrdelivery.R;
 import com.mage.ziplrdelivery.common.AppManager;
 import com.mage.ziplrdelivery.common.Data;
-import com.mage.ziplrdelivery.databinding.ActivityPasswordBinding;
+import com.mage.ziplrdelivery.databinding.ActivityVerificationBinding;
 import com.mage.ziplrdelivery.utils.Const;
 
-public class PasswordActivity extends BaseActivity implements AppManager.DataMessageListener {
+public class VerificationActivity extends BaseActivity implements AppManager.DataMessageListener, TextWatcher {
 
-    private ActivityPasswordBinding binding;
+    private ActivityVerificationBinding binding;
     private AppCompatImageView ivBack;
-    private Intent verifyIntent;
+    private String otp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(PasswordActivity.this, R.layout.activity_password);
+        binding = DataBindingUtil.setContentView(VerificationActivity.this, R.layout.activity_verification);
         initUi();
     }
 
     @Override
     protected Context getContext() {
-        return PasswordActivity.this;
+        return VerificationActivity.this;
     }
 
     @Override
     protected AppManager.DataMessageListener addDataMessageListener() {
-        return PasswordActivity.this;
+        return VerificationActivity.this;
     }
 
     @Override
@@ -47,15 +47,14 @@ public class PasswordActivity extends BaseActivity implements AppManager.DataMes
         View backView = LayoutInflater.from(this).inflate(R.layout.layout_back_arrow, binding.toolbar, false);
         ivBack = backView.findViewById(R.id.ivBack);
         getSupportActionBar().setCustomView(backView);
-        binding.collapsingTl.setTitle(getResources().getString(R.string.enter_your_password));
+        binding.collapsingTl.setTitle(getResources().getString(R.string.verification));
         binding.collapsingTl.setCollapsedTitleTextAppearance(R.style.CollapsedAppBarTitle);
         binding.collapsingTl.setExpandedTitleTextAppearance(R.style.ExpandedAppBarTitle);
         binding.collapsingTl.setCollapsedTitleTypeface(Typeface.createFromAsset(getAssets(), "font/ProximaNova-Bold.ttf"));
         binding.collapsingTl.setExpandedTitleTypeface(Typeface.createFromAsset(getAssets(), "font/ProximaNova-Bold.ttf"));
         ivBack.setOnClickListener(this);
-        binding.tvForgot.setPaintFlags(binding.tvForgot.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        binding.tvForgot.setOnClickListener(this);
-        binding.btSubmit.setOnClickListener(this);
+        binding.btResend.setOnClickListener(this);
+        binding.edOtpView.addTextChangedListener(this);
     }
 
     @Override
@@ -64,30 +63,15 @@ public class PasswordActivity extends BaseActivity implements AppManager.DataMes
             case R.id.ivBack:
                 onBackPressed();
                 break;
-            case R.id.tvForgot:
-                if (!disableClick) {
-                    disableClick = true;
-                    if(verifyIntent == null){
-                        verifyIntent = new Intent(PasswordActivity.this,VerificationActivity.class);
-                    }
-                    startActivity(verifyIntent);
-                }
-                break;
-            case R.id.btSubmit:
-                /*if (!disableClick) {
-                    disableClick = true;
-                }*/
+            case R.id.btResend:
+
                 break;
         }
     }
 
     @Override
     protected void onInternetChange(boolean isInternet) {
-        if (isInternet) {
-            binding.rlInternet.setVisibility(View.GONE);
-        } else {
-            binding.rlInternet.setVisibility(View.VISIBLE);
-        }
+
     }
 
     @Override
@@ -102,6 +86,24 @@ public class PasswordActivity extends BaseActivity implements AppManager.DataMes
 
     @Override
     public void onNewDataMessage(String from, String msg, Data data) {
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        otp = String.valueOf(charSequence);
+        if (otp.length() == 4) {
+            //Verify User
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
 
     }
 }
